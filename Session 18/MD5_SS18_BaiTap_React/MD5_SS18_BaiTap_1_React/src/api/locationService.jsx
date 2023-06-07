f65aa45f6d0f88ac9dc514434c201d1e53a511f4
Nhan Tic <78673223+trongnhan34vn@users.noreply.github.com>
2023-06-07 08:58:39 +0700

    Delete Session 18 directory

--- Session 18/MD5_SS18_BaiTap_React/MD5_SS18_BaiTap_1_React/src/api/locationService.jsx
+++
@@ -1,6 +0,0 @@
-import { instance } from "./axios"
-
-export const FIND_ALL_LOCATION = async () => {
-    let response = await instance.get('/api/register')
-    return response.data;
-}
--- Session 18/MD5_SS18_BaiTap_React/MD5_SS18_BaiTap_1_React/src/redux/reducer/index.jsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import listLocations from './locationReducer';
-import loginDetails from './userReducer';
-
-const rootReducer = combineReducers({
-    loginDetails, listLocations
-});
-
-export default rootReducer;
