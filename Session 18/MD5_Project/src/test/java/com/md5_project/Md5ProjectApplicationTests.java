4138f1bb84c34a60a693f1595809771cb0d2666f
Mac <trongnhan34vn@gmail.com>
2023-06-07 08:55:45 +0700

    update

---
+++ Session 18/MD5_Project/src/test/java/com/md5_project/Md5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md5_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
